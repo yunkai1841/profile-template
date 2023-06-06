dad3d031b813f5980e92ae07317745a1a0e68c96
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-06-06 12:46:05 +0900

    Markdown mount

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// javascript here
+import { marked } from 'marked';
+const targetArea = document.getElementById('main-content');
+
+const markdown = `
+# Hello World
+`;
+
+mountMd(markdown, targetArea);
+function mountMd(md, target) {
+  const html = marked.parse(md);
+  target.innerHTML = html;
+}
